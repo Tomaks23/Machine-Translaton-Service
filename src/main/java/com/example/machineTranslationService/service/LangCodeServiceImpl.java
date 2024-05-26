ac7cd1213c1fa594b33c12eb95886c799830754c
Tomaks23 <tomakrstic23@gmail.com>
2024-05-26 13:23:48 +0200

    Removed vulnerable library Model mapper

--- src/main/java/com/example/machineTranslationService/service/LangCodeServiceImpl.java
+++ src/main/java/com/example/machineTranslationService/service/LangCodeServiceImpl.java
@@ -3,25 +3,26 @@ package com.example.machineTranslationService.service;
 import com.example.machineTranslationService.model.LangCode;
 import com.example.machineTranslationService.model.dto.LangCodeDto;
 import com.example.machineTranslationService.repository.LangCodeRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class LangCodeServiceImpl implements LangCodeService {
 
-    @Autowired
-    private LangCodeRepository langCodeRepository;
+  @Autowired
+  private LangCodeRepository langCodeRepository;
 
-    public List<LangCodeDto> allLangCodes() {
-        List<LangCode> langCodes = langCodeRepository.findAll();
-        ModelMapper modelMapper = new ModelMapper();
-        return langCodes.stream()
-                .map(langCode -> modelMapper.map(langCode, LangCodeDto.class))
-                .toList();
-    }
+  public List<LangCodeDto> allLangCodes() {
+    List<LangCode> langCodes = langCodeRepository.findAll();
+    return langCodes.stream()
+        .map(langCode -> new LangCodeDto(langCode.getId(), langCode.getCode()))
+        .collect(Collectors.toList());
+  }
 }
 
 
